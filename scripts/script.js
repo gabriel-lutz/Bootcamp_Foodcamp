6e0470d19eb20bd998b18c63148c04d31d6aec2e
Gabriel Pere Lutz <gabriel.p.lutz@gmail.com>
2021-04-05 14:40:42 -0300

    Site finalizado (sem revisão)

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,27 @@
-
 let comida = 0
+let nomeComida;
 let bebida = 0
+let nomeBebida;
 let sobremesa = 0
+let nomeSobremesa
 let total = comida + bebida + sobremesa
 
 function selecionar(categoria, item){
    
     const deselecionar = document.querySelector(".categoria:nth-child("+ categoria +") .borda")
     if (deselecionar !== null){
-    deselecionar.classList.remove("borda")
+        deselecionar.classList.remove("borda")
     }
     const selecionado = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") ")
     selecionado.classList.add("borda")
-    
     const deselecionarIcone = document.querySelector(".categoria:nth-child("+ categoria +") .aparecer")
     if (deselecionarIcone !== null){
-    deselecionarIcone.classList.remove("aparecer")
+        deselecionarIcone.classList.remove("aparecer")
     }
     const selecionadoIcone = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") ion-icon ")
     selecionadoIcone.classList.add("aparecer")
-    
-
-   
     if(categoria === 1){
-        const nomeComida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
+        nomeComida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
         const valorComida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .valor ")
         comida = Number(valorComida.innerHTML);
         const substituirComida = document.querySelector(".comida-final")
@@ -31,7 +29,7 @@ function selecionar(categoria, item){
         const substituirValorComida = document.querySelector(".valor-comida-final")
         substituirValorComida.innerHTML = ("R$" + valorComida.innerHTML) 
     }else if(categoria === 2){
-        const nomeBebida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
+        nomeBebida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
         const valorBebida = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .valor ")
         bebida = Number(valorBebida.innerHTML);
         const substituirBebida = document.querySelector(".bebida-final")
@@ -39,7 +37,7 @@ function selecionar(categoria, item){
         const substituirValorBebida = document.querySelector(".valor-bebida-final")
         substituirValorBebida.innerHTML = ("R$" + valorBebida.innerHTML) 
     }else if(categoria === 3){
-        const nomeSobremesa = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
+        nomeSobremesa = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .nome-prato ")
         const valorSobremesa = document.querySelector(".categoria:nth-child("+ categoria +") .item:nth-child("+ item +") .valor ")
         sobremesa = Number(valorSobremesa.innerHTML);
         const substituirSobremesa = document.querySelector(".sobremesa-final")
@@ -48,10 +46,6 @@ function selecionar(categoria, item){
         substituirValorSobremesa.innerHTML = ("R$" + valorSobremesa.innerHTML) 
     }
     total = comida + bebida + sobremesa;
-    
-
-
-
     if (comida > 0 && bebida > 0 && sobremesa > 0){
         const ativarBotao = document.querySelector(".msg")
         ativarBotao.classList.add("ativar")
@@ -61,11 +55,16 @@ function selecionar(categoria, item){
     }
 }
 
-
 function confirmar(){
     if (comida > 0 && bebida > 0 && sobremesa > 0){
         const ativarBotao = document.querySelector(".confirmar-background")
         ativarBotao.classList.add("aparecer")
+        let nome = prompt("Qual o seu nome?")
+        let endereco = prompt("Qual será o endereço da entrega?")
+        const mensagemWhats = encodeURIComponent("Olá, gostaria de fazer o pedido: \n - Prato: " + nomeComida.innerHTML + "\n - Bebida: " + nomeBebida.innerHTML + "\n - Sobremesa: " + nomeSobremesa.innerHTML +"\n Total: R$ "+ total+ "\n \n Nome:" + nome + "\n Endereco:" + endereco)
+        const enviarMensagem = document.querySelector(".confirmar")
+        console.log(enviarMensagem)
+        enviarMensagem.innerHTML = ("<a href='https://wa.me/?text="+ mensagemWhats +"' target='_blank'><p>Tudo certo, pode pedir!</p></a>")
     }
 }
 
